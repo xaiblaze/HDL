a85c72e4636fcb3db6b169047b781b4d589a80f3
Xavier <31713676+xaiblaze@users.noreply.github.com>
2024-02-13 16:43:09 -0800

    Update stop_watch.vhd

--- VHDL/stop_watch.vhd
+++ VHDL/stop_watch.vhd
@@ -2,76 +2,76 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity stop_watch_tb is
-end stop_watch_tb;
+entity stop_watch is
+port
+(
+i_clk_h		: in	std_logic;
+i_sys_rst_h	: in	std_logic;
+i_start_stop_h	: in	std_logic;
+i_rst_watch_h	: in	std_logic;
+o_watch_running_h :out	std_logic;
+o_watch_rst_h	: out	std_logic
+);
 
-architecture RTL of stop_watch_tb is
+end stop_watch;
 
-	constant clkh_per : time := 100 ns; -- 100MHz
-	
-	signal i_clk_h		: 	std_logic :='0';
-	signal i_sys_rst_h	: 	std_logic :='0';
-	signal i_start_stop_h	: 	std_logic :='0';
-	signal i_rst_watch_h	: 	std_logic :='0';
-	signal o_watch_running_h :	std_logic := '0';
-	signal o_watch_rst_h	: 	std_logic :='0';
-	
+architecture RTL of stop_watch is
+
+type state_type is (zero, counting, stop, S1_dummy);
+signal cur_state, next_state : state_type;
+
+
+begin
+
+	mechanism : process (i_clk_h, i_sys_rst_h) is
 	begin
+		if(i_sys_rst_h = '1') then
+			cur_state <= zero; -- this works but it doens't set the state variables correctly?
+
+		elsif Rising_Edge(i_clk_h) then
+			cur_state <= next_state;
+
+		end if;
 	
-	stop_watch : entity work.stop_watch
-	port map
-	(
-	i_clk_h => i_clk_h,
-	i_sys_rst_h => i_sys_rst_h,
-	i_start_stop_h => i_start_stop_h,
-	i_rst_watch_h => i_rst_watch_h,
-	o_watch_running_h => o_watch_running_h,
-	o_watch_rst_h => o_watch_rst_h
-	);
-	
-	i_clk_h <= not i_clk_h after 5 ns;
-
-	stim: process is
-	begin
-		i_start_stop_h <= '1';
-		wait for 15 ns;
-		i_start_stop_h <= '0';
-		wait for 50 ns;
-
-		i_rst_watch_h <= '1';
-		wait for 20 ns;
-		i_rst_watch_h <= '0';
-		wait for 50 ns;
-
-		i_start_stop_h <= '1';
-		wait for 15 ns;
-		i_start_stop_h <= '0';
-		wait for 50 ns;		
-
-		i_sys_rst_h <= '1';
-		wait for 20 ns;
-
-		i_start_stop_h <= '1';
-		wait for 15 ns;
-		i_start_stop_h <= '0';
-		wait for 50 ns;		
-
-		i_sys_rst_h <= '0';
-		wait for 20 ns;
-
-		i_start_stop_h <= '1';
-		wait for 15 ns;
-		i_start_stop_h <= '0';
-		wait for 50 ns;	
-
-		wait;
-
-
 	end process;
 
+	state_proc : process (i_start_stop_h, i_rst_watch_h) is
+	begin
+		if (i_rst_watch_h = '1') or (i_sys_rst_h = '1') then -- added this OR here to force state variables to reset
+			o_watch_running_h <= '0';
+			o_watch_rst_h <= '1';
+			next_state <= zero;
+		else
+			case cur_state is
+				when zero =>
+					o_watch_running_h <= '0';	
+					o_watch_rst_h <= '1';
+					if Rising_Edge(i_start_stop_h) then
+						next_state <= counting;
+					end if;
+				when counting =>
+					o_watch_running_h <= '1';
+					o_watch_rst_h <= '0';
+					if Rising_Edge(i_start_stop_h) then
+						next_state <= stop;
+					end if;
+				when stop =>
+					o_watch_running_h <= '0';
+					o_watch_rst_h <= '0';
+					if Rising_Edge(i_start_stop_h) then
+						next_state <= counting;
+					end if;
+				when others =>
+					o_watch_running_h <= '0';
+					o_watch_rst_h <= '0';
+					next_state <= zero;
 
+			end case;
+		end if;
+	end process;
 
 	
-	
+
 end RTL;
